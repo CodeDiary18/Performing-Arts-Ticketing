eca24ab0fe11441d8db5737b66645c8c5fe2f6cd
CodeDiary18 <codediary18@gmail.com>
2025-02-25 00:10:13 +0900

    chore: JPA entity scan config

--- infra/src/main/kotlin/com/cd18/infra/persistence/config/JpaConfig.kt
+++ infra/src/main/kotlin/com/cd18/infra/persistence/config/JpaConfig.kt
@@ -1,8 +1,10 @@
 package com.cd18.infra.persistence.config
 
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @Configuration
 @EnableJpaRepositories(basePackages = ["com.cd18.infra.persistence.repository.jpa"])
+@EntityScan(basePackages = ["com.cd18.infra.persistence.model"])
 class JpaConfig
