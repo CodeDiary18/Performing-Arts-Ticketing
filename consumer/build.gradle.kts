9940e501563bee8a68e239eb8c954be5712fceda
CodeDiary18 <codediary18@gmail.com>
2025-06-14 01:25:13 +0900

    chore: configures consumer for ticket processing

--- consumer/build.gradle.kts
+++ consumer/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 dependencies {
     implementation(project(":common"))
     implementation(project(":domain"))
+    implementation(project(":infra"))
 
     implementation ("org.springframework.boot:spring-boot-starter-batch")
     implementation("org.springframework.kafka:spring-kafka:3.3.5")
@@ -14,4 +15,6 @@ dependencies {
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation ("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation ("org.jetbrains.kotlin:kotlin-reflect")
 }
--- consumer/src/main/kotlin/com/cd18/consumer/ConsumerApplication.kt
+++ consumer/src/main/kotlin/com/cd18/consumer/ConsumerApplication.kt
@@ -1,11 +1,10 @@
 package com.cd18.consumer
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
 @SpringBootApplication(
-    exclude = [DataSourceAutoConfiguration::class],
+    scanBasePackages = ["com.cd18"],
 )
 class ConsumerApplication
 
